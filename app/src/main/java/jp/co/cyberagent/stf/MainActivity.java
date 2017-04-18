e4bd9fc1d9e1c4b9345c7a4c036f28008ac1dac6
杨帆 <yangfan@51xianqu.net>
2017-04-18 10:05:02 +0800

    增加图标

---
+++ app/src/main/java/jp/co/cyberagent/stf/MainActivity.java
@@ -0,0 +1,22 @@
+package jp.co.cyberagent.stf;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import jp.co.cyberagent.stf.R;
+
+/**
+ * @author Ivonhoe on 2017/3/31.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+}
